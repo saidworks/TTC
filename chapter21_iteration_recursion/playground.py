16e14e2525ea650d4c669198deea131fcc6bd9da
Said ZITOUNI <zitouni.sd@gmail.com>
2021-05-15 21:33:46 +0000

    create venv and quick sort algo from pseudo code

--- chapter21_iteration_recursion/playground.py
+++ chapter21_iteration_recursion/playground.py
@@ -10,28 +10,60 @@ countdown(6)
     # compare element of the two lists
         # sort then again in one list
     # return the ordered list (set)
-test = [(-i)**i for i in range(50)]
+test = [i*i for i in range(50)]
 
-<<<<<<< HEAD
+
 # Divide and conquer => Taking one large data set and dividing it into subsets that we handle independently
-=======
 def divideAndSort(ensemble):
     if len(ensemble)<2:
         return ensemble
     #create subsets
     else:
         length = len(ensemble) // 2
-        ensemble_1 = ensemble[:length]
-        ensemble_2 = ensemble[length:]
-    for i in range(1,length):
-        temp = ensemble_1[i-1]
-        if ensemble_1[i] < temp:
-            ensemble_1[i-1] = ensemble_1[i]
-            ensemble_1[i] = temp
-        elif ensemble_1[i] > temp:
-            ensemble_1[i] = ensemble_1[i]
+        L1 = ensemble[:length]
+        L2 = ensemble[length:]
+        i = 0
+        j = 0
+        k = 0
+        while (j < len(L1)) or (k < len(L2)):
+            if j < len(L1):
+                if k < len(L2):
+                    # we are not at the end of L1 or L2, so pull the smaller value
+                    if L1[j] < L2[k]:
+                        ensemble[i] = L1[j]
+                        j += 1
+                    else:
+                        ensemble[i] = L2[k]
+                        k += 1
+                else:
+                    # we are at the end of L2, so just pull from L1
+                    ensemble[i] = L1[j]
+                    j += 1
+            else:
+                # we are at the end of L1, so just pull from L2
+                ensemble[i] = L2[k]
+                k += 1
+            i += 1
+        return ensemble
 
-    return ensemble_1
-
-print(divideAndSort(test))
->>>>>>> 61c311fedeccb5e2626d101ae30236c58855ee23
+# Quick Sort
+    #pick the pivot
+    #form two list one (L1) with number bigger than pivot and (L2) other less than it
+    #quick sort L1 and L2
+    #join two list and pivot
+import random
+def quickSort(L):
+    if len(L) < 2:
+        return L
+    else:
+        index = random.randint(0,len(L)//2)
+        pivot = L[index]
+        L1 = [i for i in L if i<pivot]
+        L2 = [i for i in L if i>pivot]
+    quickSort(L1)
+    quickSort(L2)
+    L = L1
+    L.append(pivot)
+    L.extend(L2)
+    return L
+print(quickSort(test))
