61c311fedeccb5e2626d101ae30236c58855ee23
Said <zitouni.sd@gmail.com>
2021-05-15 20:41:59 +0000

    divide code

--- chapter21_iteration_recursion/playground.py
+++ chapter21_iteration_recursion/playground.py
@@ -4,4 +4,30 @@ def countdown(n):
         countdown(n-1)
 
 countdown(6)
+# check if list or set contain more than one element
+    # divide one set to two subsets
+    # sort each list
+    # compare element of the two lists
+        # sort then again in one list
+    # return the ordered list (set)
+test = [(-i)**i for i in range(50)]
 
+def divideAndSort(ensemble):
+    if len(ensemble)<2:
+        return ensemble
+    #create subsets
+    else:
+        length = len(ensemble) // 2
+        ensemble_1 = ensemble[:length]
+        ensemble_2 = ensemble[length:]
+    for i in range(1,length):
+        temp = ensemble_1[i-1]
+        if ensemble_1[i] < temp:
+            ensemble_1[i-1] = ensemble_1[i]
+            ensemble_1[i] = temp
+        elif ensemble_1[i] > temp:
+            ensemble_1[i] = ensemble_1[i]
+
+    return ensemble_1
+
+print(divideAndSort(test))
